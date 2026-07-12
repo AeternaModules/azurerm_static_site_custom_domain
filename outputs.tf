52363c632ebeeb9a49a1ba4e4b3d1dbbb4151bd4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,18 @@
-output "static_site_custom_domains" {
-  description = "All static_site_custom_domain resources"
-  value       = azurerm_static_site_custom_domain.static_site_custom_domains
-  sensitive   = true
-}
 output "static_site_custom_domains_domain_name" {
-  description = "List of domain_name values across all static_site_custom_domains"
-  value       = [for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : v.domain_name]
+  description = "Map of domain_name values across all static_site_custom_domains, keyed the same as var.static_site_custom_domains"
+  value       = { for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : k => v.domain_name }
 }
 output "static_site_custom_domains_static_site_id" {
-  description = "List of static_site_id values across all static_site_custom_domains"
-  value       = [for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : v.static_site_id]
+  description = "Map of static_site_id values across all static_site_custom_domains, keyed the same as var.static_site_custom_domains"
+  value       = { for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : k => v.static_site_id }
 }
 output "static_site_custom_domains_validation_token" {
-  description = "List of validation_token values across all static_site_custom_domains"
-  value       = [for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : v.validation_token]
+  description = "Map of validation_token values across all static_site_custom_domains, keyed the same as var.static_site_custom_domains"
+  value       = { for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : k => v.validation_token }
   sensitive   = true
 }
 output "static_site_custom_domains_validation_type" {
-  description = "List of validation_type values across all static_site_custom_domains"
-  value       = [for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : v.validation_type]
+  description = "Map of validation_type values across all static_site_custom_domains, keyed the same as var.static_site_custom_domains"
+  value       = { for k, v in azurerm_static_site_custom_domain.static_site_custom_domains : k => v.validation_type }
 }
 
